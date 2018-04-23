5209c0eb48e6715ec9ac5643defaf63374f26a85
sankettps <sanket.tps@gmail.com>
2018-04-23 23:13:28 +0530

    edit coin form added, myprofile page added, launcher heading added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,23 @@ class ApplicationController < ActionController::Base
   before_action :set_news_feed
 
   def set_news_feed
-  	xml = HTTParty.get("https://cointelegraph.com/rss").body
-		if xml.present?
-			@feed = Feedjira::Feed.parse(xml) rescue nil
 			@news_feeds = {}
-			if @feed.present?
-				@feed.entries.first(2).each_with_index do |feed,index|
-					@news_feeds["#{index}"]={}
-					@news_feeds["#{index}"]["title"] = feed.title
-					@news_feeds["#{index}"]["description"] = feed.summary
-					@news_feeds["#{index}"]["published_at"] = feed.published
-					@news_feeds["#{index}"]["image"] = feed.image
-					# jQuery = RubyCheerio.new(feed.summary)
-					# @news_feeds["#{index}"]["image"] = jQuery.find('img')[0].prop('img','src')
-				end
-			end
-		end
+  # 	xml = HTTParty.get("https://cointelegraph.com/rss").body
+		# if xml.present?
+		# 	@feed = Feedjira::Feed.parse(xml) rescue nil
+		# 	@news_feeds = {}
+		# 	if @feed.present?
+		# 		@feed.entries.first(2).each_with_index do |feed,index|
+		# 			@news_feeds["#{index}"]={}
+		# 			@news_feeds["#{index}"]["title"] = feed.title
+		# 			@news_feeds["#{index}"]["description"] = feed.summary
+		# 			@news_feeds["#{index}"]["published_at"] = feed.published
+		# 			@news_feeds["#{index}"]["image"] = feed.image
+		# 			# jQuery = RubyCheerio.new(feed.summary)
+		# 			# @news_feeds["#{index}"]["image"] = jQuery.find('img')[0].prop('img','src')
+		# 		end
+		# 	end
+		# end
   end
 
   protected
